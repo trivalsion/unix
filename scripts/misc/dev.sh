7f676ec1e62861d3163968ec0513e1352f086989
trivalsion <98325881+trivalsion@users.noreply.github.com>
2023-05-31 18:19:07 +0200

    Update dev.sh

--- scripts/misc/dev.sh
+++ scripts/misc/dev.sh
@@ -2,7 +2,7 @@
 ## Shell script which sets up the OS for development/programming
 
 
-# Setup git repos, if github does not allow password aunthentication use [this guide](https://github.com/trivalsion/programming/blob/master/documents/guides/git.md)
+## Setup git repos, if github does not allow password aunthentication use [this guide](https://github.com/trivalsion/programming/blob/master/documents/guides/git.md) ##
 cd $HOME/git-repos || exit
 mkdir -p github
 cd github || exit
@@ -12,12 +12,14 @@ git clone https://github.com/trivalsion/unix
 git clone https://github.com/trivalsion/notes
 
 
-# Configure git settings
+
+## Configure git settings ##
 git config --global credential.helper store # saves the git account password when you go to some repo and do a push or pull(make sure to do a push/pull where account and password is requested) and enter the password, now all of the next times, the password won't be requested(the account and password will be stored in - ~/.git-credentials)
 git config --global user.name "eltertrias"
 git config --global user.email "eltertrias"
 
 
-# Add local binary directory to PATH - optional
+
+## Add local binary directory to PATH - optional ##
 # mkdir -p $HOME/opt/bin $HOME/opt/src
 # echo 'PATH=$PATH:$HOME/opt/bin' >> $HOME/.bashrc
